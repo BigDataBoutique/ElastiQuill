60d7c1839ea26ec0646735d37c8dc936ed59f37d
Tigran Simonyan <tigran.simonyan.1996@gmail.com>
2019-04-19 09:05:08 +0000

    Image upload error and api errors returning HTML fix

--- admin-frontend/src/App.js
+++ admin-frontend/src/App.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 import React, { Component } from 'react';
--- admin-frontend/src/components/Layout.js
+++ admin-frontend/src/components/Layout.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {ToastContainer} from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
 
 export const Layout = (props) => (
 	<>
