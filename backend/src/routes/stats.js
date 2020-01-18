9280f1d623c417b22e2620f7a993ae3abf6cf6b3
Itamar Syn-Hershko <itamar@bigdataboutique.com>
2020-01-18 23:44:10 +0200

    linter

--- backend/src/routes/stats.js
+++ backend/src/routes/stats.js
@@ -30,14 +30,14 @@ router.get(
 
     let most_viewed_post = preparePost(stats.most_viewed_post) || {};
     stats.most_viewed_post = {
-    	title: most_viewed_post.title,
-    	url: most_viewed_post.url,
-    	full_url: most_viewed_post.full_url,
-    	published_at: most_viewed_post.published_at,
-    	comments_count: most_viewed_post.comments_count,
-    	views_count: most_viewed_post.views_count,
-    	visitors_count: most_viewed_post.visitors_count,
-	};
+      title: most_viewed_post.title,
+      url: most_viewed_post.url,
+      full_url: most_viewed_post.full_url,
+      published_at: most_viewed_post.published_at,
+      comments_count: most_viewed_post.comments_count,
+      views_count: most_viewed_post.views_count,
+      visitors_count: most_viewed_post.visitors_count,
+    };
     stats.comments_by_date = commentsByDate;
     stats.popular_posts = stats.popular_posts.map(preparePost);
 
